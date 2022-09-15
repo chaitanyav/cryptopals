c2ff0f3dd7b917c63f259a34746965b6e67641f0
NagaChaitanya Vellanki <pnagato@protonmail.com>
2022-09-15 01:05:15 -0700

    Add base64 to ascii decoding

--- src/base64.rs
+++ src/base64.rs
@@ -21,7 +21,9 @@ fn binary_string_to_u8(input: String) -> u8 {
     let mut factor = 1;
     for ch in input.chars().rev() {
         result += if ch == '1' { factor } else { 0 };
-        factor *= 2;
+        if factor < 128 {
+            factor *= 2;
+        }
     }
     result
 }
@@ -39,7 +41,7 @@ fn char_to_u8(ch: char) -> u8 {
     }
 }
 
-pub fn binary_string_to_base64(mut bin_string: String) -> String {
+fn u8_to_base64_char() -> HashMap<u8, char> {
     let mut mapping: HashMap<u8, char> = HashMap::new();
 
     for (idx, val) in ('A'..='Z').enumerate() {
@@ -57,6 +59,32 @@ pub fn binary_string_to_base64(mut bin_string: String) -> String {
     mapping.insert(62, '+');
     mapping.insert(63, '/');
 
+    mapping
+}
+
+fn base64_char_to_u8() -> HashMap<char, u8> {
+    let mut mapping: HashMap<char, u8> = HashMap::new();
+
+    for (idx, val) in ('A'..='Z').enumerate() {
+        mapping.insert(val, idx as u8);
+    }
+
+    for (idx, val) in ('a'..='z').enumerate() {
+        mapping.insert(val, (idx + 26) as u8);
+    }
+
+    for (idx, val) in ('0'..='9').enumerate() {
+        mapping.insert(val, (idx + 52) as u8);
+    }
+
+    mapping.insert('+', 62);
+    mapping.insert('/', 63);
+
+    mapping
+}
+
+pub fn binary_string_to_base64(mut bin_string: String) -> String {
+    let mapping = u8_to_base64_char();
     bin_string = pad_zeroes(bin_string);
 
     let mut base64_string = String::new();
@@ -87,4 +115,62 @@ pub fn hex_to_base64(input: &str) -> String {
     }
 
     binary_string_to_base64(bin_string)
+}
+
+fn base64_to_ascii_without_padding(input: String) -> String {
+    let mut bin_string = String::new();
+
+    let mapping = base64_char_to_u8();
+    for ch in input.chars() {
+        bin_string.push_str(&format!("{:06b}", *mapping.get(&ch).unwrap()));
+    }
+
+    let mut idx = 0;
+    let mut ascii_string = String::new();
+    while idx < bin_string.len() {
+        let temp_str = &bin_string[idx..(idx + 8)];
+        ascii_string.push_str(&format!(
+            "{}",
+            binary_string_to_u8(temp_str.to_string()) as char
+        ));
+        idx += 8;
+    }
+
+    ascii_string
+}
+
+fn base64_to_ascii_with_padding(input: String) -> String {
+    let mut bin_string = String::new();
+
+    let mapping = base64_char_to_u8();
+    for ch in input.chars() {
+        if ch != '=' {
+            bin_string.push_str(&format!("{:06b}", *mapping.get(&ch).unwrap()));
+        }
+    }
+
+    while bin_string.len() % 8 != 0 {
+        bin_string.push('0');
+    }
+
+    let mut idx = 0;
+    let mut ascii_string = String::new();
+    while idx < bin_string.len() {
+        let temp_str = &bin_string[idx..(idx + 8)];
+        ascii_string.push_str(&format!(
+            "{}",
+            binary_string_to_u8(temp_str.to_string()) as char
+        ));
+        idx += 8;
+    }
+
+    ascii_string
+}
+
+pub fn base64_to_ascii(input: String) -> String {
+    if input.ends_with("=") {
+        base64_to_ascii_with_padding(input)
+    } else {
+        base64_to_ascii_without_padding(input)
+    }
 }
